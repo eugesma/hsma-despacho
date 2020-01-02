787c6267587d84c277dcc45fd8a50b507c63d459
BasHud <eugesma@gmail.com>
2020-01-02 09:00:38 -0300

    Change login view

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module Hsma
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.time_zone = 'Buenos Aires'
+    config.active_record.default_timezone = :utc
   end
 end
